648b4f5731da40123f29d873c342ff59b7d000e4
James Petersen <j.petersen@unb.ca>
2023-11-17 08:41:37 -0400

    Adjust FinancialDocument fields to match database

--- src/cs2043group10/data/FinancialDocument.java
+++ src/cs2043group10/data/FinancialDocument.java
@@ -1,19 +1,23 @@
 package cs2043group10.data;
 
+import java.util.Optional;
+
 public class FinancialDocument {
 	public final int documentId;
 	public final int patientId;
-	public final long amount;
+	public final int amount;
+	public final Optional<Integer> amountPaid;
 	public final String description;
 	public final String title;
 	public final long createTimestamp;
 	
-	public FinancialDocument(int documentId, int patientId, long amount, String description, long createTimestamp, String title) {
+	public FinancialDocument(int documentId, int patientId, int amount, String description, long createTimestamp, String title, Optional<Integer> amountPaid) {
 		this.documentId = documentId;
 		this.patientId = patientId;
 		this.amount = amount;
 		this.description = description;
 		this.createTimestamp = createTimestamp;
 		this.title = title;
+		this.amountPaid = amountPaid;
 	}
 }
