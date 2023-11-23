2ad0b0f9493ef367b1f5d67954cf0a20ff3ce143
tifeodd <145512364+tifeodd@users.noreply.github.com>
2023-11-23 13:57:26 -0400

    Update MedicalDocumentView.java

--- src/cs2043group10/views/MedicalDocumentView.java
+++ src/cs2043group10/views/MedicalDocumentView.java
@@ -32,14 +32,59 @@ public class MedicalDocumentView implements IReversable {
 	}
 	
 	private static void createView() {
-		// TODO
+		view = new GridPane();
+        view.setPadding(new Insets(10));
+        view.setHgap(10);
+        view.setVgap(10);
+
+        
+        Label titleLabel = new Label("Title: ");
+        Label docIdLabel = new Label("Document ID: " );
+        Label patientIdLabel = new Label("Patient ID: " );
+        Label typeLabel = new Label("Type: " );
+        Label bodyLabel = new Label("Body: " );
+        Label auxiliaryLabel = new Label("Auxiliary: " );
+        Label createdAtLabel = new Label("Created At: " );
+        Label modifiedAtLabel = new Label("Modified At: " );
+       
+	view.addColumn(0,titleLabel,docIdLabel,patientIdLabel,typeLabel,bodyLabel,auxiliaryLabel,createdAtLabel,modifiedAtLabel );
+        
+
+        Button editButton = new Button("Edit");
+        editButton.setOnAction(e -> {
+            
+        });
+        view.add(editButton, 1, 7);
+
+        // Adjust column constraints as needed
+
+        Stage primaryStage = new Stage();
+        Scene scene = new Scene(view, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Medical Document Details");
+        primaryStage.show();
+    }
+
+		
+		
 	}
 
 	@Override
 	public void beforeShow() {
-		// TODO Auto-generated method stub
+	Label titleLabel = new Label("Title: " + data.title);
+        Label docIdLabel = new Label("Document ID: " + data.documentId);
+        Label patientIdLabel = new Label("Patient ID: " + data.patientId);
+        Label typeLabel = new Label("Type: " + data.type);
+        Label bodyLabel = new Label("Body: " + data.body);
+        Label auxiliaryLabel = new Label("Auxiliary: " + data.auxiliary);
+        Label createdAtLabel = new Label("Created At: " + data.createTimestamp);
+        Label modifiedAtLabel = new Label("Modified At: " + data.modifyTimestamp);
 		
 	}
+
+	
+
+	
 
 	@Override
 	public void afterHide() {
