cc7de67dc2de782b88db02b95c3370b106475da5
tifeodd <145512364+tifeodd@users.noreply.github.com>
2023-11-23 13:19:06 -0400

    Update FinancialDocumentView.java

--- src/cs2043group10/views/FinancialDocumentView.java
+++ src/cs2043group10/views/FinancialDocumentView.java
@@ -32,7 +32,27 @@ public class FinancialDocumentView implements IReversable {
 	}
 	
 	private static void createView() {
-		// TODO
+	private static VBox createView(FinancialDocument financialDocument) {
+    VBox view = new VBox(10);
+    view.setPadding(new Insets(10));
+
+    Label titleLabel = new Label("Title: " + financialDocument.title);
+    Label idLabel = new Label("Document ID: " + financialDocument.documentId);
+    Label patientIdLabel = new Label("Patient ID: " + financialDocument.patientId);
+    Label amountLabel = new Label("Amount: $" + financialDocument.amount);
+    Label descriptionLabel = new Label("Description: " + financialDocument.description);
+    Label timestampLabel = new Label("Created At: " + new Date(financialDocument.createTimestamp));
+    Label amountPaidLabel = new Label("Amount Paid: " + (financialDocument.amountPaid.isPresent() ? "$" + financialDocument.amountPaid.get() : "Not paid"));
+
+    // Style labels
+    Stream.of(titleLabel, idLabel, patientIdLabel, amountLabel, descriptionLabel, timestampLabel, amountPaidLabel)
+            .forEach(label -> label.setStyle("-fx-font-weight: bold"));
+
+    view.getChildren().addAll(titleLabel, idLabel, patientIdLabel, amountLabel, descriptionLabel, timestampLabel, amountPaidLabel);
+
+    return view;
+}
+
 	}
 
 	@Override
